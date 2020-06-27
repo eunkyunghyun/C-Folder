66b0540b037f810598984c39eb1e6f0b7ff35d84
eunkyunghyun <57312000+eunkyunghyun@users.noreply.github.com>
2020-06-27 20:23:16 +0900

    Add files via upload

---
+++ Algorithm/Project/전역 변수의 초기값과 생존 기간.c
@@ -0,0 +1,17 @@
+/* #include <stdio.h>
+int count;
+
+void counter()
+{
+	count = 20;
+}
+
+int main(void)
+{
+	printf("count = %d\n", count);
+	counter();
+	printf("count = %d\n", count);
+
+	return 0;
+}
+*/
