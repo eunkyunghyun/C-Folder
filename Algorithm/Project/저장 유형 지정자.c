70ea3ee8694ddd9d10573d29a6b2fd1244171c17
eunkyunghyun <57312000+eunkyunghyun@users.noreply.github.com>
2020-06-27 20:39:03 +0900

    Add files via upload

---
+++ Algorithm/Project/저장 유형 지정자.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+void type(void);
+
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 3; i++)
+		type();
+
+	return 0;
+}
+
+void type(void)
+{
+	auto int auto_count = 0;
+	static int static_count = 0;
+
+	auto_count++;
+	static_count++;
+	
+	printf("auto_count = %d\n", auto_count);
+	printf("static_count = %d\n", static_count);
+}
---
+++ Algorithm/Project/전역 변수 오류 예제.c
@@ -0,0 +1,19 @@
+/* #include <stdio.h>
+void print(void);
+
+int i;
+
+int main(void)
+{
+	for (i = 0; i < 5; i++)
+		f();
+
+	return 0;
+}
+
+void print(void)
+{
+	for (i = 0; i < 10; i++)
+		printf("#");
+}
+*/
