34e1daa2b37322286849eff1c14f5062743ffc2d
eunkyunghyun <57312000+eunkyunghyun@users.noreply.github.com>
2020-07-04 22:23:29 +0900

    Add files via upload

---
+++ Algorithm/Project/이중 포인터 예제 2.c
@@ -0,0 +1,17 @@
+/* #include <stdio.h>
+void set_pointer(char** q);
+char* proverb = "All that glisters is not gold.";
+
+int main(void)
+{
+	char* p = "zzz";
+	set_pointer(&p);
+	printf("%s\n", p);
+	return 0;
+}
+
+void set_pointer(char** q)
+{
+	*q = proverb;
+}
+*/
