409411f4f2f4d9299210d853a2272ba2d0a56b17
eunkyunghyun <57312000+eunkyunghyun@users.noreply.github.com>
2020-07-12 21:31:34 +0900

    Add files via upload

---
+++ Algorithm/Project/문자 변환.c
@@ -0,0 +1,17 @@
+/* #include <stdio.h>
+#include <ctype.h>
+
+int main(void)
+{
+	int str;
+
+	while ((str = getchar()) != EOF)
+	{
+		if (islower(str))
+			str = toupper(str);
+		putchar(str);
+	}
+
+	return 0;
+}
+*/
