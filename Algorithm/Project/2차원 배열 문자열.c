7c8ce838d58d4f6fcc3e279f9f539874cbcaedae
eunkyunghyun <57312000+eunkyunghyun@users.noreply.github.com>
2020-07-12 22:19:16 +0900

    Add files via upload

---
+++ Algorithm/Project/2차원 배열 문자열.c
@@ -0,0 +1,19 @@
+/* #include <stdio.h>
+
+int main(void)
+{
+	int i;
+	char fruits[3][20];
+	
+	for (i = 0; i < 3; i++)
+	{
+		printf("과일 이름을 입력하세요: ", fruits[i]);
+		scanf_s("%s", fruits[i]);
+	}
+
+	for (i = 1; i <= 3; i++)
+		printf("%d번째 과일: %s\n", i, fruits[i-1]);
+
+	return 0;
+}
+*/
