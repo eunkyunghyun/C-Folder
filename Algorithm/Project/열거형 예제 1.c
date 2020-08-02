6a659733dc9103ff74884330cd1205a6eb72b7a5
eunkyunghyun <57312000+eunkyunghyun@users.noreply.github.com>
2020-08-02 21:34:37 +0900

    Add files via upload

---
+++ Algorithm/Project/열거형 예제 1.c
@@ -0,0 +1,16 @@
+/* #include <stdio.h>
+
+enum days {SUN, MON, TUE, WED, THU, FRI, SAT};
+char* days_name[] = { "일요일", "월요일", "화요일", "수요일", "목요일", "금요일", "토요일" };
+
+int main(void)
+{
+	enum days d;
+
+	d = TUE;
+
+	printf("%s은 %d번째 요일입니다.", days_name[d], d);
+
+	return 0;
+}
+*/
