b4868d97e3adbc61d352f92bcca0a425cc15259a
eunkyunghyun <57312000+eunkyunghyun@users.noreply.github.com>
2020-08-15 12:21:24 +0900

    Add files via upload

---
+++ Assignment/1차원 배열/소스.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <string.h>
+
+int main(void)
+{
+    int a, b, c;
+    int mul;
+    int count[10] = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+    char num[100];
+
+    scanf("%d %d %d", &a, &b, &c);
+    mul = a * b * c;
+    sprintf(num, "%d", mul);
+
+    for (int i = 0; i < strlen(num); i++)
+    {
+        for (int j = 0; j <= 9; j++)
+        {
+            if (j == (int)num[i])
+                count[j] += 1;
+        }
+        printf("%d\n", strlen(num));
+        printf("%d\n", (int)num[i]);
+    }
+
+    return 0;
+}
