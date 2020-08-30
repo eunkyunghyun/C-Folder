14d3713447238539ccf1db2646325f5c213c2794
eunkyunghyun <57312000+eunkyunghyun@users.noreply.github.com>
2020-08-30 21:53:59 +0900

    Update 연결 리스트 예제 2.c

--- Basic/array/연결 리스트 예제 2.c
+++ Basic/array/연결 리스트 예제 2.c
@@ -16,7 +16,7 @@ int main(void)
 	head->next = node1;
 
 	printf("도서 제목을 입력하세요: ");
-	scanf("%s", &node1->name[100]);
+	scanf("%s", &node1->name);
 
 	printf("출판 연도를 입력하세요: ");
 	scanf("%d", &node1->year);
@@ -25,7 +25,7 @@ int main(void)
 	node1->next = node2;
 	
 	printf("도서 제목을 입력하세요: ");
-	scanf("%s", &node2->name[100]);
+	scanf("%s", &node2->name);
 
 	printf("출판 연도를 입력하세요: ");
 	scanf("%d", &node2->year);
@@ -36,7 +36,7 @@ int main(void)
 
 	while (curr != NULL)
 	{
-		printf("도서 제목: %s, 출판 연도: %d\n", curr->name[100], curr->year);
+		printf("도서 제목: %s, 출판 연도: %d\n", curr->name, curr->year);
 		curr = curr->next;
 	}
 
