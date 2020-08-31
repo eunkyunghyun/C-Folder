0a4644e74348e8c1f1130f01dbfef4d4654c6050
eunkyunghyun <57312000+eunkyunghyun@users.noreply.github.com>
2020-08-31 10:12:07 +0900

    Create if 예제 2.c

---
+++ Basic/condition/if/if 예제 2.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int year;
+  
+	printf("연도를 입력하세요: ");
+	scanf_s("%d", &year);
+  
+	if (year % 4 == 0 && year % 100 != 0 && year % 400 == 0)
+		printf("윤년입니다.\n");
+	else
+		printf("윤년이 아닙니다.\n");
+    
+	return 0;
+}
