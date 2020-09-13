05413ed7c298894d96812c93c56265078ff3e2b7
eunkyunghyun <57312000+eunkyunghyun@users.noreply.github.com>
2020-09-13 21:04:21 +0900

    Update Absolute_Value.c

--- Theory/Basic/Absolute_Value.c
+++ Theory/Basic/Absolute_Value.c
@@ -15,3 +15,17 @@ int main(void)
 
 	return 0;
 }
+
+#include <stdio.h>
+
+int main(void)
+{
+	int value;
+
+	printf("Please enter one integer: ");
+	scanf("%d", &value);
+
+	printf("absolute value = %d\n", (value > 0) ? value : -value);
+
+	return 0;
+}
