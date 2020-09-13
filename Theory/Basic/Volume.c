c54f6f731925a50e26ce455f91dd6ad7aca83024
eunkyunghyun <57312000+eunkyunghyun@users.noreply.github.com>
2020-09-13 21:11:53 +0900

    Update Volume.c

--- Theory/Basic/Volume.c
+++ Theory/Basic/Volume.c
@@ -7,7 +7,7 @@ int main(void)
 	printf("Please enter the radius: ");
 	scanf("%lf", &radius);
 
-	printf("volume = %f\n", 4 / 3 * 3.14 * radius * radius * radius);
+	printf("volume = %f\n", 4.0 / 3.0 * 3.14 * radius * radius * radius);
 
 	return 0;
 }
