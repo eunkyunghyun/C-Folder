37e9df3c260ebac02c0c0412ffc5e185347422bd
eunkyunghyun <57312000+eunkyunghyun@users.noreply.github.com>
2020-09-13 21:50:48 +0900

    Create Letter_Estimation.c

---
+++ Theory/Basic/Letter_Estimation.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(void)
+{
+	char letter;
+
+	printf("Please enter the letter: ");
+	scanf("%c", &letter);
+
+	if ((int)letter >= 65 && (int)letter <= 90)
+		printf("capital letter");
+	else if ((int)letter >= 97 && (int)letter <= 122)
+		printf("small letter");
+	else if ((int)letter >= 48 && (int)letter <= 57)
+		printf("number");
+
+	return 0;
+}
