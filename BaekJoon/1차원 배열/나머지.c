b3c60024a218b928127179bbf4b014a6aee0e992
eunkyunghyun <57312000+eunkyunghyun@users.noreply.github.com>
2020-09-19 17:53:22 +0900

    Update 나머지.c

--- BaekJoon/1차원 배열/나머지.c
+++ BaekJoon/1차원 배열/나머지.c
@@ -5,27 +5,27 @@ int main(void)
 	int num;
 	int count;
 	int sum = 0;
-	int num1[100] = { 0, };
+	int arr[100] = { 0, };
 
 	for (int i = 0; i < 10; i++)
 	{
-		scanf_s("%d", &num);
-		num1[i] = num % 42;
+		scanf("%d", &num);
+		arr[i] = num % 42;
 	}
 
 	for (int i = 0; i < 10; i++)
 	{
-		count = num1[i];
+		count = arr[i];
 		for (int j = i + 1; j < 10; j++)
 		{
-			if (count == num1[j])
-				num1[j] = -1;
+			if (count == arr[j])
+				arr[j] = -1;
 		}
 	}
 
 	for (int i = 0; i < 10; i++)
 	{
-		if (num1[i] != -1)
+		if (arr[i] != -1)
 			sum++;
 	}
 
