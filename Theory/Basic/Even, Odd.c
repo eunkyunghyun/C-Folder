d80495dc321447473339e8a989d556628fe945c8
eunkyunghyun <57312000+eunkyunghyun@users.noreply.github.com>
2020-09-27 21:48:38 +0900

    Create  Even, Odd.c

---
+++ Theory/Basic/Even, Odd.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int arr[10];
+	int even = 0;
+	int odd = 0;
+
+	for (int i = 0; i < sizeof(arr) / sizeof(int); i++)
+	{
+		printf("Please enter an element: ");
+		scanf("%d", &arr[i]);
+	}
+
+	for (int i = 0; i < sizeof(arr) / sizeof(int); i++)
+	{
+		if (arr[i] % 2 == 0)
+		{
+			even += arr[i];
+		}
+		else
+			odd += arr[i];
+	}
+
+	printf("even = %d\n", even);
+	printf("odd = %d\n", odd);
+
+	return 0;
+}
