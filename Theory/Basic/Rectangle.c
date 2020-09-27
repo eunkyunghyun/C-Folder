3cbc086010fab0dfb062edb1ad54ae0c3318301c
eunkyunghyun <57312000+eunkyunghyun@users.noreply.github.com>
2020-09-27 21:01:51 +0900

    Create Rectangle.c

---
+++ Theory/Basic/Rectangle.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int width, height;
+
+	printf("Please enter width and height: ");
+	scanf("%d %d", &width, &height);
+
+	for (int i = 0; i < height; i++)
+	{
+		for (int j = 0; j < width; j++)
+		{
+			printf("*");
+		}
+		printf("\n");
+	}
+
+	return 0;
+}
