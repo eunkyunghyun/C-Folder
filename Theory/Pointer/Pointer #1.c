b1fba77547123f50bcdeb6e9efeaad6a0d4dd7c3
eunkyunghyun <57312000+eunkyunghyun@users.noreply.github.com>
2020-11-29 19:58:33 +0900

    Update Pointer #1.c

--- Theory/Pointer/Pointer #1.c
+++ Theory/Pointer/Pointer #1.c
@@ -2,14 +2,14 @@
 
 int main(void)
 {
-	int x = 20;
-	int* p1 = &x;
+	int value = 20;
+	int* p1 = &value;
 	int* p2 = p1;
 
 	(*p1)++;
 	(*p2)--;
 
-	printf("%d", x);
+	printf("%d", value);
 
 	return 0;
 }
